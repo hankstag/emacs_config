e375418be534735acf795a4e0e3164d9661aba99
Hank Shen <sunshen.hx@gmail.com>
2018-01-26 11:30:27 -0500

    upload elpa

---
+++ elpa/reveal-in-osx-finder-20150802.957/reveal-in-osx-finder-autoloads.el
@@ -0,0 +1,28 @@
+;;; reveal-in-osx-finder-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "reveal-in-osx-finder" "reveal-in-osx-finder.el"
+;;;;;;  (23147 19338 289553 645000))
+;;; Generated autoloads from reveal-in-osx-finder.el
+
+(autoload 'reveal-in-osx-finder "reveal-in-osx-finder" "\
+Reveal the file associated with the current buffer in the OS X Finder.
+In a dired buffer, it will open the current directory.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("reveal-in-osx-finder-pkg.el") (23147
+;;;;;;  19319 244388 73000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; reveal-in-osx-finder-autoloads.el ends here
