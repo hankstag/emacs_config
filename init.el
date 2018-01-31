f66d279cb0934c1a3eb0781ca641d96ae9669a43
hanxiao <sunshen.hx@gmail.com>
2018-01-30 20:27:26 -0500

    update scrolling

--- init.el
+++ init.el
@@ -17,6 +17,17 @@
 (require 'smooth-scrolling)
 (smooth-scrolling-mode)
 
+(defun smooth-scroll (increment)
+  (scroll-up increment) (sit-for 0.05)
+  (scroll-up increment) (sit-for 0.02)
+  (scroll-up increment) (sit-for 0.02)
+  (scroll-up increment) (sit-for 0.05)
+  (scroll-up increment) (sit-for 0.06)
+  (scroll-up increment))
+
+(global-set-key [(mouse-5)] '(lambda () (interactive) (smooth-scroll 1)))
+(global-set-key [(mouse-4)] '(lambda () (interactive) (smooth-scroll -1)))
+
 ;;(require 'powerline)
 ;;(powerline-default-theme)
 (require 'spaceline-config)
